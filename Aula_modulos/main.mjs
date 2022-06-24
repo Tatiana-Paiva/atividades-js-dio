5f7d3f829ecc36d95147c551afe5e8aa037d0cd4
Tatiana-Paiva <boutiquepinkmood@gmail.com>
2022-06-24 18:10:25 -0300

    Atividade Módulo - Import e Expport

---
+++ Aula_modulos/main.mjs
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Juliana', 29));
+console.log(mostraCidade('Matheus', 'Rio de Janeiro'));
+console.log(mostraHobby('Jéssica', 'Conhecer novos lugares'))
+
